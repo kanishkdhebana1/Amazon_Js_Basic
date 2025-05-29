7ccd769e14efa28dc78c8e57d6ac0687c8b12460
kanishkdhebana <dhebanakanishk@gmail.com>
2025-05-29 13:57:39 +0530

    inheritance part done(for products.js)

--- data/products.js
+++ data/products.js
@@ -37,6 +37,28 @@ class Product {
   getPrice() {
     return `$${formatCurrency(this.priceCents)}` ;
   }
+
+  extraInfoHTML() {
+    return "" ;
+  }
+}
+
+
+class Clothing extends Product {
+  sizeChartLink ;
+
+  constructor(productDetails) {
+    super(productDetails) ;
+    this.sizeChartLink = productDetails.sizeChartLink ;
+  }
+
+  extraInfoHTML() {
+    return `
+      <a href = "${this.sizeChartLink}" target = "_blank"> 
+        Size Chart
+      </a>
+    ` ;
+  }
 }
 
 
@@ -700,5 +722,9 @@ export const products = [
     ]
   }
 ].map((productDetails) => {
+  if (productDetails.type === 'clothing') {
+    return new Clothing(productDetails) ;
+  } 
+
   return new Product(productDetails) ;
 }) ;

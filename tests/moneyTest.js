6ff8835de2c8d63274ea500c7fcf4519a3e267ac
kanishkdhebana <dhebanakanishk@gmail.com>
2025-05-29 10:43:35 +0530

    added jasmine testing framework

---
+++ tests/moneyTest.js
@@ -0,0 +1,10 @@
+import { formatCurrency } from "../scripts/utils/money.js";
+
+
+if (formatCurrency(2095) === '20.95') {
+    console.log("Format Currency Passed") ;
+}
+
+else {
+    console.log("Format Currency Passed") ;
+}

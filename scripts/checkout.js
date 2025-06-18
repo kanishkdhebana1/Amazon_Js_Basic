5a61c410a82b264cef96b80204ad99de77ef118a
kanishkdhebana <dhebanakanishk@gmail.com>
2025-06-18 18:20:13 +0530

    learn promises

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -3,14 +3,23 @@ import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import '../data/cart-class.js'
 import '../data/backend-practice.js'
 import { loadProducts } from "../data/products.js";
+import { loadCart } from "../data/cart.js" ;
 
-new Promise((resolve) => {
-    loadProducts(() => {
-        resolve() ;
-    }) ;
-}).then(() => {
+Promise.all([
+    new Promise((resolve) => {
+        loadProducts(() => {
+            resolve() ;
+        }) ;
+    }),
+
+    new Promise((resolve) => {
+        loadCart(() => {
+            resolve() ;
+        }) ;
+    })
+
+]).then(() => {
     renderOrderSummary() ;
     renderPaymentSummary() ;
 }) ;
-
 

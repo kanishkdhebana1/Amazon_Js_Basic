c3e638c3466dd3846cb50e7b5d7e12adcec66054
kanishkdhebana <dhebanakanishk@gmail.com>
2025-06-18 17:40:07 +0530

    fallback branch if something goes wrong

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,16 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js" ;
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import '../data/cart-class.js'
+import '../data/backend-practice.js'
+import { loadProducts } from "../data/products.js";
 
-renderOrderSummary() ;
-renderPaymentSummary() ;
+new Promise((resolve) => {
+    loadProducts(() => {
+        resolve() ;
+    }) ;
+}).then(() => {
+    renderOrderSummary() ;
+    renderPaymentSummary() ;
+}) ;
+
+
